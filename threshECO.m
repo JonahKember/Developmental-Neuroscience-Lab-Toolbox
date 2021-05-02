5c5e9d34d480935cc1438ff870956a91fb76ec9b
Jonah Kember <81769550+JonahKember@users.noreply.github.com>
2021-05-02 09:33:30 -0400

    Add files via upload

---
+++ threshECO.m
@@ -0,0 +1,13 @@
+function p = threshECO(nNodes)
+%
+%   Data-driven approach to selecting a proportional threshold. Chooses the threshold that maximizes 
+%   the trade-off between wiring cost and overall efficieny (sum of global and average local efficiency). 
+%   Analytic and empirical results have demonstrated that in EEG functional networks, this value can
+%   selected a priori using only the number of nodes.
+%
+%   De Vico Fallani, F., Latora, V., & Chavez, M. (2017). A Topological Criterion for Filtering 
+%   Information in Complex Brain Networks. PLoS Computational Biology, 13(1), e1005305–e1005305.
+%
+%%
+p = 3/(nNodes - 1);
+end
