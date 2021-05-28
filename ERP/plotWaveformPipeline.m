3a29483956551f5f900cdb02d615ffa1dbdb1541
Jonah Kember <81769550+JonahKember@users.noreply.github.com>
2021-05-28 14:56:45 -0400

    Add files via upload

---
+++ ERP/plotWaveformPipeline.m
@@ -0,0 +1,16 @@
+%% Plot Grand Averaged Waveforms
+
+GACond1 = grandAverageERP(Cond1);
+GACond2 = grandAverageERP(Cond2);
+
+cfg = [];
+cfg.channels =           [12,14,15,16,17,18];                    % Channels to be averaged across in plot.
+cfg.ylim =               [-10 10];                               % [Min Max] y-axis limit (Voltage).
+cfg.colour =             'yes';                                  % Plot in colour or in black ('yes'/'no').
+cfg.labels =             'yes';                                  % Include labels or not ('yes'/'no').
+cfg.legend =             'no';                                   % Include legend or not ('yes'/'no').    
+cfg.condNames =          {'Condition 1', 'Condition 1'};         % Names to be included in Legend.
+cfg.highlightEffect =    'yes';                                  % Include box around time window.
+cfg.effect =             [.2 .5;.6 .7];                          % Time window(s) for boxes.
+
+waveformERP(cfg,GACond1,GACond2)
---
+++ ERP/waveformERP.m
@@ -0,0 +1,29 @@
+function  waveformERP(cfg,GACond1,GACond2)
+
+if contains(cfg.colour,'yes')
+    plot(GACond1.time,mean(GACond1.avg(cfg.channels,:),1))
+    hold on
+    plot(GACond1.time,mean(GACond2.avg(cfg.channels,:),1))
+else
+    plot(GACond1.time,mean(GACond1.avg(cfg.channels,:),1),'k')
+    hold on
+    plot(GACond1.time,mean(GACond2.avg(cfg.channels,:),1),'k','LineStyle','-');
+end
+
+if contains(cfg.labels,'yes')
+xlabel('Time (ms)')
+ylabel ('Voltage (uV)')
+end
+
+if contains(cfg.legend,'yes')
+legend(cfg.condNames{1},cfg.condNames{2})
+end
+ylim(cfg.ylim)
+
+if contains(cfg.highlightEffect,'yes')
+    for n = 1:size(cfg.effect,1)
+        rectangle('Position',[cfg.effect(n,1) cfg.ylim(1) (cfg.effect(n,2) - cfg.effect(n,1))  (cfg.ylim(2) - cfg.ylim(1))])
+    end
+end
+
+end
