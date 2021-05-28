52bdb293c997e2aca12d10908b65e7fc89929fee
Jonah Kember <81769550+JonahKember@users.noreply.github.com>
2021-05-28 11:34:28 -0400

    Update collectParticipants.m

--- ERP/collectParticipants.m
+++ ERP/collectParticipants.m
@@ -1,13 +1,12 @@
-function [allSubjCond1,allSubjCond2] = collectParticipants(subjNums,fileLocationCond1,fileLocationCond2)
+function [allSubjCond1,allSubjCond2] = collectParticipants(subjects,fileNameCond1,fileNameCond2)
 
-nSubj = length(subjNums);
-allSubjCond1 = zeros(1,nSubj);
-allSubjCond2 = zeros(1,nSubj);
+nSubj = length(subjects);
 
 for s = 1:nSubj
-   inputfileCond1 = @(id)fileLocationCond1; 
-   inputfileCond2 = @(id)fileLocationCond2;
-   allSubjCond1{s} = load(inputfileCond1(subjNums{s})); 
-   allSubjCond2{s} = load(inputfileCond2(subjNums{s}));
-end
+   erpCond1 = struct2cell(load(fileNameCond1(subjects{s}))); 
+   allSubjCond1{s} = erpCond1{1, 1};
+   
+   erpCond2 = struct2cell(load(fileNameCond2(subjects{s}))); 
+   allSubjCond2{s} = erpCond2{1, 1};
+ end
 end
