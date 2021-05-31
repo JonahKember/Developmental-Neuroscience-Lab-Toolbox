4b254851176cc018a9715f37f81e6469eb28c8d3
Jonah Kember <81769550+JonahKember@users.noreply.github.com>
2021-05-31 12:11:30 -0400

    Update collectParticipants.m

--- ERP/collectParticipants.m
+++ ERP/collectParticipants.m
@@ -1,12 +1,60 @@
-function [allSubjCond1,allSubjCond2] = collectParticipants(subjects,fileNameCond1,fileNameCond2)
+function [allSubjCond1,allSubjCond2] = collectParticipants(fileLocation,cond1,cond2)
 
-nSubj = length(subjects);
+directory = struct2cell(dir(fileLocation));
 
-for s = 1:nSubj
-   Cond1 = struct2cell(load(fileNameCond1(subjects{s}))); 
-   allSubjCond1{s} = Cond1{1, 1};
-   
-   Cond2 = struct2cell(load(fileNameCond2(subjects{s}))); 
-   allSubjCond2{s} = Cond2{1, 1};
- end
+for n = 1:length(directory)
+    TF(n) = contains(directory(1,n),'.mat');
+end
+
+filenames = directory(1,find(TF == 1)); %#ok<FNDSB>
+
+condition1 = [];
+condition2 = [];
+
+if contains(cond2,cond1)
+    for n = 1:length(filenames)
+        if contains(filenames(n),cond1)
+            if ~contains(filenames(n),cond2)
+            condition1 = [condition1,filenames(n)]; %#ok<*AGROW>
+            end
+        end
+        if contains(filenames(n),cond2) 
+        condition2 = [condition2,filenames(n)];
+        end
+    end
+end
+
+if contains(cond1,cond2)
+    for n = 1:length(filenames)
+        if contains(filenames(n),cond2)
+            if ~contains(filenames(n),cond1)
+            condition2 = [condition2,filenames(n)];
+            end
+        end
+        if contains(filenames(n),cond1) 
+        condition1 = [condition1,filenames(n)];
+        end
+    end
+end
+
+if ~contains(cond1,cond2)
+   if ~contains(cond2,cond1)
+    for n = 1:length(filenames)
+        if contains(filenames(n),cond1)
+            condition1 = [condition1,filenames(n)];
+        end
+        if contains(filenames(n),cond2) 
+        condition2 = [condition2,filenames(n)];
+        end
+    end
+   end
+end
+
+for s = 1:length(condition1)
+    allSubjCond1{1,s} = load(condition1{s});
+end
+
+for s = 1:length(condition2)
+   allSubjCond2{1,s} = load(condition2{s});
+end
 end
