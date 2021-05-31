f096b1af68b20ee0338505bdc0ac1bdc01f421a4
Jonah Kember <81769550+JonahKember@users.noreply.github.com>
2021-05-31 12:11:51 -0400

    Update clusterStatsPipeline.m

--- ERP/clusterStatsPipeline.m
+++ ERP/clusterStatsPipeline.m
@@ -1,23 +1,21 @@
 %% Collect Participants to be Used in Statistics Test
 
-subjects = {'1','2','3','4'};                                               % Change this so it corresponds to each of the participant's numbers.
-
 fileLocation = 'C:\Data';                                                   % Specify the file location of your data.
-fileNameCond1 = @(id)[fileLocation,'Subj_',id,'Cond1'];                     % Rewrite 'Subj_',id,'Cond1' so it matches how the data is saved ('id' should 
-fileNameCond2 = @(id)[fileLocation,'Subj_',id,'Cond2'];                     % be in place of the subject number). Do this for both conditions. 
+Condition1 = 'Go';                                                          % Specify the names of each condition, as they're saved.
+Condition2 = 'NoGo';
 
-[Cond1,Cond2] = collectParticipants(subjects,fileNameCond1,fileNameCond2); 
+[C1,C2] = collectParticipants(fileLocation,Condition1,Condition2);
 
 %% Run Cluster-Based Permutation Test
 
 cfg = [];
 cfg.testType = 'erp';                                                       % Type of statistict test: can be 'erp' or 'power'
 cfg.avgOverTime = 'yes';                                                    % Average over a time window, either 'yes' or 'no'.
-cfg.timeWindow = [.300 .500];                                               % The time window used in the test. 
+cfg.timeWindow = [.300 .500];                                               % The time window used in the test (in Seconds). 
 cfg.freqWindow = [8 12];                                                    % Frequency range used in test (if cfg.dataType = 'power')
 cfg.neighbours = load('Neuroscan64ChanNeighbours');                         % The neighbours used in the satistics test. Alternatively, 'Geodesic128ChanNeighbours'.
 
-statistic = clusterStats(cfg,Cond1,Cond2);                                  % Format of test is Cond1 - Cond2.
+statistic = clusterStats(cfg,C1,C2);                                        % Format of test is Cond1 - Cond2.
 readStat(statistic,.05);                                                    % See whether there are any effects with a p-value < .05.
 
 %% Plot 
