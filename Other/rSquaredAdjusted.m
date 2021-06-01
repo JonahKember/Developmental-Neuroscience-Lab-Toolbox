fc616051909dc59761f9f22abc422906d0476779
Jonah Kember <81769550+JonahKember@users.noreply.github.com>
2021-06-01 12:18:23 -0400

    Update rSquaredAdjusted.m

--- Other/rSquaredAdjusted.m
+++ Other/rSquaredAdjusted.m
@@ -1,25 +1,16 @@
 function rSquaredAdjusted = rSquaredAdjusted(y,predictedY,p)
-
 TSS = [];
-
     for n = 1:length(y)
         SS = (y(n) - mean(y))^2;
         TSS = [TSS;SS];
     end
-    
 TSS = sum(TSS);
-
 SSE = [];
-
     for n = 1:length(y)
         SE = (y(n) - predictedY(n))^2; 
         SSE = [SSE;SE];
     end
-    
 SSE = sum(SSE);
 n = length(y);
- 
 rSquaredAdjusted = 1 - ((n - 1)/(n - p))*(SSE/TSS);
-
 end
-
