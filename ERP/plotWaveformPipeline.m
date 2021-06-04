f9e4b8cdb2faf2860554adbd793ebfcfdcb07de1
Jonah Kember <81769550+JonahKember@users.noreply.github.com>
2021-06-04 14:28:43 -0400

    Update plotWaveformPipeline.m

--- ERP/plotWaveformPipeline.m
+++ ERP/plotWaveformPipeline.m
@@ -4,7 +4,7 @@ GACond1 = grandAverageERP(Cond1);
 GACond2 = grandAverageERP(Cond2);
 
 cfg = [];
-cfg.channels =           [12,14,15,16,17,18];                    % Channels to be averaged across in plot.
+cfg.channels =           [12,14,15,16,17,18];                    % Channels to be averaged across in plot. Can also be channel names: {'POz','FCz','Cz'}
 cfg.ylim =               [-10 10];                               % [Min Max] y-axis limit (Voltage).
 cfg.colour =             'yes';                                  % Plot in colour or in black ('yes'/'no').
 cfg.labels =             'yes';                                  % Include labels or not ('yes'/'no').
