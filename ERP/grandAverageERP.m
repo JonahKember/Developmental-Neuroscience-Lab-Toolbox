5d1a0ca5045762606e5a7f2a7744ffeecf3d9058
Jonah Kember <81769550+JonahKember@users.noreply.github.com>
2021-06-04 14:30:56 -0400

    Update grandAverageERP.m

--- ERP/grandAverageERP.m
+++ ERP/grandAverageERP.m
@@ -1,7 +1,10 @@
 function GA = grandAverageERP(allSubj)
+
 GA = allSubj{1,1}.avg;
+
 for n = 2:length(allSubj)
  GA = GA + allSubj{1,n}.avg;
 end
+GA.label = allSubj{1,1}.label;
 GA = GA/length(allSubj);
 end
